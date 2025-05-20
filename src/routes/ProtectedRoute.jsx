f9f448c64713d60caeb2f98bb2fcd5c25dea2e4f
Pedro Ramos <pedromelloramos@gmail.com>
2025-05-19 21:58:37 -0300

    Loading animation
    Using echarts-for-react

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -2,12 +2,55 @@ import { Navigate, useLocation } from "react-router-dom";
 import { auth } from '../firebase';
 import { onAuthStateChanged, getAuth } from "firebase/auth";
 import { useEffect, useState } from "react";
+import ReactECharts from 'echarts-for-react';
 
 export default function ProtectedRoute({ children }) {
   const location = useLocation();
   const [loading, setLoading] = useState(true);
   const [user, setUser] = useState(null);
   const auth = getAuth();
+
+  const option = {
+    graphic: {
+      elements: [
+        {
+          type: 'group',
+          left: 'center',
+          top: 'center',
+          children: new Array(7).fill(0).map((val, i) => ({
+            type: 'rect',
+            x: i * 20,
+            shape: {
+              x: 0,
+              y: -40,
+              width: 10,
+              height: 80
+            },
+            style: {
+              fill: '#5470c6'
+            },
+            keyframeAnimation: {
+              duration: 150,
+              delay: i * 40,
+              loop: true,
+              keyframes: [
+                {
+                  percent: 0.5,
+                  scaleY: 0.3,
+                  easing: 'cubicIn'
+                },
+                {
+                  percent: 1,
+                  scaleY: 1,
+                  easing: 'cubicOut'
+                }
+              ]
+            }
+          }))
+        }
+      ]
+    }
+  };
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
@@ -18,7 +61,7 @@ export default function ProtectedRoute({ children }) {
   }, [auth]);
 
   if (loading) {
-    return <div className="text-center mt-10 text-gray-500">Loading...</div>;
+    return <div className="text-center mt-10 text-gray-500"><ReactECharts option={option} /></div>;
   }
 
   if (!user) {
